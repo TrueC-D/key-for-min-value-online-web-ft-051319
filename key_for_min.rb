49b52f54c94b70ecee0c238ca066b5529fa51975
Alethea Cary <aletheacary@gmail.com>
2019-05-19 21:10:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,9 +5,11 @@ require "pry"
 def key_for_min_value(name_hash)
   min_price = nil
   min_name = nil
-  if min_price > price
-    min_price = price
+  
   name_hash.each do |name, price| 
+    if min_price > price || min_price == nil
+    min_price = price
+    min_name = name
     binding.pry
   end
   # x if price <= name_hash.all?
