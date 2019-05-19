a82f3b1a3ce9e7ee86ebb17dd24aa1b3ed3047fe
Alethea Cary <aletheacary@gmail.com>
2019-05-19 21:02:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,8 +3,9 @@ require "pry"
 # Given a hash with numeric values, return the key for the smallest value
  
 def key_for_min_value(name_hash)
-  min_price = nil
+  min_price = 0
   min_name = nil
+  
   name_hash.each do |name, price| 
     binding.pry
   end
